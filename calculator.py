6871c673afc2b9c3c71750c1d8ae71f983cc31d0
Derek muriuki <98864170+dee-mee@users.noreply.github.com>
2024-01-25 14:55:03 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,25 @@
 #this project showcases my skills after finishing python
 #essentials 1 in cisco
-def operations(multiplication, addition, subtraction, division):
-  return operations()
+def operations(num1=0, num2=0):
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-print("what operation would you like to perform.")
+    print("Operations include\n1.Addition,\n2.Subtraction,\n3.Multiplication,\n4.Division")
+    choice = input("choose an opeartion based on its number.")
+    if choice == '1' :
+        print("The sum is ",num1 + num2)
+    elif choice == '2':
+        print("The result of Subtraction is ",num1 - num2)
+    elif choice == '3':
+        print("The result of the operation is ,", num1 * num2)
+    elif choice == '4':
+        while num1 > 1:
+            try:
+                print("The result of Division is ",num1 / num2)
+                break
+            except ZeroError:
+                print("Please enter a VALUE bigger than that.")
+    else:
+        print("That operation is currectly not available")
+
 operations()
