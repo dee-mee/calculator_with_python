4d5bc0a35bfc24264bcd85ebe3fd0c88a84b25ed
Derek muriuki <98864170+dee-mee@users.noreply.github.com>
2024-01-25 13:59:45 +0300

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,2 +1,7 @@
 #this project showcases my skills after finishing python
 #essentials 1 in cisco
+def operations(multiplication, addition, subtraction, division):
+  return operations()
+
+print("what operation would you like to perform.")
+operations()
